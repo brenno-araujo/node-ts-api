3372240684740b36fb8f3400a87f2857824ad1f1
Brenno Araújo <brennoaraujo@yahoo.com>
2022-07-02 23:10:28 -0300

    feat: forecastController

---
+++ test/globals.d.ts
@@ -0,0 +1,7 @@
+declare var testRequest: import("supertest").SuperTest<import("supertest").Test>;
+
+// declare namespace NodeJS {
+//     interface Global {
+//         testRequest: import{'supertest'}.SuperTest<import('supertest').Test>;
+//     }
+// }
